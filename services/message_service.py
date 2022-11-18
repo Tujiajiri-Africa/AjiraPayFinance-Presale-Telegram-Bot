0ad184c312806950e74f8f61105e38b99d680072
dickensodera <dickens@laevitas.ch>
2022-11-18 04:51:56 +0300

    update: Added contract event listener

--- services/message_service.py
+++ services/message_service.py
@@ -2,15 +2,33 @@ from py_singleton import singleton
 import traceback
 import asyncio
 from utils.logger import send_error_message_to_slack
+from utils.consts import WEB3_CONSTS
+from artifacts.abi.presale_abi import presale_contract_abi
+
+import json
+import time
+from web3 import Web3
+
+infura_rpc = ''
+web3 = Web3(Web3.HTTPProvider(infura_rpc))
+
+contract = web3.eth.contract(address=web3.toChecksumAddress(WEB3_CONSTS.presale_contract_address), 
+                                abi=presale_contract_abi)
 
 @singleton
 class MessageService(object):
-    pass
-
     def handle_new_presale_token_purchase(event):
-        pass
+        try:
+            result = json.loads(Web3.toJSON(event))
 
-    async def listen_to_new_trade_position_event(event_filter, poll_interval):
+            tx_hash = result['transactionHash']
+            print(tx_hash)
+
+        except Exception as e:
+            print(traceback.print_exc())
+            send_error_message_to_slack('@everyone ' + traceback.format_exc())
+
+    async def listen_to_new_token_purchase_event(event_filter, poll_interval):
         while True:
             try:
                 for Contribute in event_filter.get_new_entries():
@@ -28,3 +46,28 @@ class MessageService(object):
             except Exception as e:
                 print(traceback.print_exc())
                 send_error_message_to_slack('@everyone ' + traceback.format_exc())
+
+
+async def main():
+    print('begin bot...')
+
+    new_ajirap_pay_presale_purchase_event_filter = contract.events.Contribute.createFilter(fromBlock='latest')
+
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                MessageService.listen_to_new_token_purchase_event(new_ajirap_pay_presale_purchase_event_filter, 2)
+            ))
+
+    except Exception as e:
+        print(traceback.print_exc())
+        send_error_message_to_slack('@everyone ' + traceback.format_exc())
+    finally:
+        loop.close()
+
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ utils/consts.py
@@ -0,0 +1,9 @@
+from py_singleton import singleton
+
+@singleton
+class WEB3_CONSTS(object):
+    aclhemy_url = 'https://polygon-mumbai.g.alchemy.com/v2/XXGBKxwudOEfk0lt_F0hICAzFO5b_zt4'
+    presale_contract_address = '0x70ab9C214818560f6Fd63d9AF9C38cF4D37Fe5A0'
+
+class SLACK_CONSTS(object):
+    slack_webhook_url = 'https://hooks.slack.com/services/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'
